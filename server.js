dd6abd32d7b6da768b1582c198c6c992842f2f1a
JClair21 <jessicagoodling@gmail.com>
2022-04-06 11:14:58 -0400

    testing api's

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
+//tells mongoose which database to connect to-MONGODB_URI IS ENVIRONMENT USED WHEN DEPLOYED TO HEROKU
 mongoose.connect(process.env.MONGODB_URI || 'mongodb: localhost: 27017/pizza-hunt' , {
     userNewUrlParser: true,
     useUifiedTopology: true
@@ -19,4 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb: localhost: 27017/pizza-hun
 //Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`🌍 Connected on localhost:${PORT}`);
+});
+
