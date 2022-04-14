c376391865c3fa4bd300cd4c4265ce6e9a91d891
JClair21 <jessicagoodling@gmail.com>
2022-04-14 15:09:06 -0400

    added endpoints for testing routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,9 @@
 const router = require('express').Router();
-//Import all of the API routes from /api/index.js(no need for index.js though since it's implied)
-const apiRoutes = require('./api');
-const htmlRoutes = require('./html/html-routes');
 
-//add prefix of '/api' to all of the api routes imported from the 'api' directory
-router.use('/api', apiRoutes);
-router.use('/', htmlRoutes);
+const pizzaRoutes = require('./pizza-routes');
 
-router.use((req, res) => {
-    res.status(404).send('<h1>😝 404 Error!</h1>');
-});
+
+//add prefix of '/pizzas' to routes created in 'pizza-routes.js'
+router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
